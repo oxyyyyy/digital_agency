0c7b20b396991757f7a0528bfc135fb12e699392
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-04-12 14:11:26 +0300

    Added two sections

--- dist/js/my_main_scripts.js
+++ dist/js/my_main_scripts.js
@@ -1,1 +1,12 @@
-new WOW().init();
+// WOW.js initialization
+$(window).on('load', function() {
+    new WOW().init();
+});
+
+// Preloader
+$(window).on('load', function() {
+    var $preloader = $('#page-preloader'),
+        $spinner = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
